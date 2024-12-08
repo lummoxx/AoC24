2fa4c91b4de78abe388c0e15c82c61999b070d5a
lummoxx <clarasalberg@live.com>
2024-12-08 05:58:04 +0100

    regex

--- day3.py
+++ day3.py
@@ -1,32 +1,21 @@
 
 import math
+import re
 
 file = open('input_files/3.txt', 'r')
 lines = file.read()
 
-def find_all(a_str, sub):
-    start = 0
-    while True:
-        start = a_str.find(sub, start)
-        if start == -1: return
-        yield start
-        start += len(sub) 
-
-
 def muls(ls : str) -> int:
     result = 0
-    mul =""
-    search = True
-    idxs = find_all(ls, "mul(")
-    for idx in idxs:
-        idx2 = ls.find(")", idx)
-        mul = ls[idx+4:idx2]
-        nums = mul.split(',')
-        if all(map(str.isdigit, nums)):
-            result = result + math.prod(map(int, nums))
+    pattern = r"mul\([0-9]+,[0-9]+\)"
+    muls = re.findall(pattern, ls)
+    for m in muls:
+        # factors = map(int, m.replace("mul(", "").replace(")", "").split(","))
+        factors = map(int, re.sub(r"mul\(|\)", "", m).split(","))
+        result = result + math.prod(factors)
     return result
-
 final = 0
+
 
 donts = lines.split("don't()")
 final = final + muls(donts[0])
