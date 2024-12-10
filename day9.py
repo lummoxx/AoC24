2773458f3a3a2906737a9d6e8aebfb0754eef861
lummoxx <clarasalberg@live.com>
2024-12-10 19:30:26 +0100

    faster

--- day9.py
+++ day9.py
@@ -1,81 +1,65 @@
-import copy
-file = open('input_files/9.txt', 'r').read()
+from collections import namedtuple
+input_file = open("input_files/9.txt", "r").read()
 
-blocks = []
-id = 0
-
-for n,c in enumerate(file):
-    if n % 2 == 0:
-        for i in range(0,int(c)):
-            blocks.append(str(id))
-        id += 1
-    else:
-        for i in range(0,int(c)):
-            blocks.append(".")
-
-reversed_blocks = copy.deepcopy(blocks)
-reversed_blocks.reverse()
-
-def part_1(blocks : list) -> list:
-    to_remove = 0
-    for i,c in enumerate(reversed_blocks):
-        if c != "." :
-            if blocks.count(".") > 0:
-                next = blocks.index(".")
-                blocks[next] = c
-                to_remove += 1
-            else: 
-                break
-    return blocks[:len(blocks)-to_remove]
-
-def remove_moved_file(f: list, i:int):
-    dots = ["."] * len(f) 
-    blocks[len(blocks)-i:len(blocks)-i+len(f)] = dots
-
-def find_empty_slot(i : int, last : int)-> tuple[int,int]:
-    i = blocks.index(".", i)
-    j = i
-    while(i < last and blocks[i+1] == "."):
-        i += 1
-    return (j,i+1)
-
-def next_file(i : int) -> tuple[int, list]:
-    while reversed_blocks[i] == ".":
-        i += 1
-    f = [reversed_blocks[i]]
-    i +=1
-    while(i < len(reversed_blocks) and reversed_blocks[i] == f[0]):
-        f.append(reversed_blocks[i])
-        i += 1
-    return i, f
-
-def part_2(blocks : list) -> list:
-    (x,y) = find_empty_slot(0, len(blocks))
-    i, f = next_file(0)
-    while (i < len(reversed_blocks)):
-        if y-x >= len(f):
-            blocks[x:x+len(f)] = f
-            remove_moved_file(f, i)
-            i, f = next_file(i)
-            if blocks[0:len(blocks)-i].count(".")>0:
-                (x,y) = find_empty_slot(0,len(blocks)-i)
-            else: 
-                break
+def part_1() :
+    blocks, id, result = [], 0, 0
+    for n,c in enumerate(input_file):
+        if n % 2 == 0:
+            for i in range(0,int(c)):
+                blocks.append(str(id))
+            id += 1
         else:
-            if blocks[y:len(blocks)-i].count(".")>0:
-                (x,y) = find_empty_slot(y, len(blocks)-i)
-            else:
-                if (i < len(reversed_blocks)):
-                    i, f = next_file(i)
-                    (x,y) = find_empty_slot(0, len(blocks)-i)
-                else: 
-                    break
-    return blocks
-                
-result = 0
-for i, c in enumerate(part_1(blocks)):
-    if c != ".":
-        result += i * int(c) 
+            for i in range(0,int(c)):
+                blocks.append(".")
+    i, j = 0, len(blocks)-1
+    while(j > i):
+        if blocks[j] != ".":
+            if blocks[i] == ".":
+                blocks[i], blocks[j] = blocks[j], blocks[i]
+                j -= 1
+            i += 1
+        else:
+            j -= 1
+    for i, c in enumerate(blocks):
+        if c != ".":
+            result += i * int(c) 
+    print("Part 1: ", result)
 
-print(result)
+
+File = namedtuple("File", ["id", "size"])
+def parse_files() -> list:
+    files = []
+    id = 0
+    for i,c in enumerate(input_file):
+        if i % 2 == 0:
+            files.append(File(str(id), int(c)))
+            id += 1
+        else:
+            files.append(File("empty", int(c)))
+    return files
+
+def part_2():
+    files = parse_files()
+    for f in range(len(files)-1, 0, -1):
+        move = files[f]
+        if move.id != "empty":
+            try: 
+                slot, val = next(((s, v) for s, v in enumerate(files) if v.id == "empty" and v.size >= move.size and s < f), (None, None))
+            except StopIteration: 
+                slot, val = None, None
+            if slot is not None and val is not None:
+                files[f] = File("empty", move.size)
+                if val.size > move.size:
+                    files.insert(slot+1, File("empty", val.size-move.size))
+                files[slot] = move
+    result, index = 0, 0
+    for file in files:
+        if file.id != "empty":
+            for i in range(0, file.size):
+                result += (index+i) *  int(file.id)
+        index += file.size
+    print("Part 2: ", result)
+
+part_1()
+part_2()
 
