8ac5dba566181315efe5ea28114a9259107aa292
Clara Salberg <clara.salberg@consultant.volvo.com>
2024-12-16 14:01:20 +0100

    name changes

--- day16.py
+++ day16.py
@@ -2,7 +2,8 @@
 import copy
 import sys
 from collections import defaultdict
-lines = open('input_files/tst2.txt', 'r').read().splitlines()
+
+lines = open('input_files/16.txt', 'r').read().splitlines()
 sys.setrecursionlimit(sys.getrecursionlimit()*10)
 
 directions = {"^":(-1,0), ">":(0,1), "v":(1,0), "<":(0,-1)}
@@ -12,8 +13,8 @@ start = 0,0
 walls = defaultdict(lambda:False)
 start_direction = ">"
 
-for i,l in enumerate(lines):
-    for j,c in enumerate(l):
+for i,line in enumerate(lines):
+    for j,c in enumerate(line):
         if c == "E":
             goal = i,j
         elif c == "S":
@@ -21,55 +22,55 @@ for i,l in enumerate(lines):
         elif c == "#":
             walls[(i,j)] = True
 
-distance_matrix = [[float('inf') for _ in range(len(lines[0]))] for _ in range(len(lines))]
-distance_matrix[start[0]][start[1]] = 0
+lowest_cost = [[float('inf') for _ in range(len(lines[0]))] for _ in range(len(lines))]
+lowest_cost[start[0]][start[1]] = 0
 
-def cost(new_direction : str, old_direction : str):
+def step_cost(new_direction : str, old_direction : str):
     if new_direction == old_direction:
         return 1
     return 1001
 
-def add_surrounding_distances(start : tuple[int, int], direction : str):
-    new_spots = []
+def find_neighbours(start : tuple[int, int], direction : str):
+    neighbours = []
     (x,y) = start
     for d in ["^",">","<", "v"] :
-        c = cost(d, direction)
+        cost = step_cost(d, direction)
         (dx, dy) = directions[d]
-        if not walls[(x+dx, y+dy)] and ((distance_matrix[x][y] + c) < distance_matrix[x+dx][y+dy]):
-            distance_matrix[x+dx][y+dy] = distance_matrix[x][y] + c
-            new_spots.append(((x+dx,y+dy), d))
-    return new_spots
+        if not walls[(x+dx, y+dy)] and ((lowest_cost[x][y] + cost) < lowest_cost[x+dx][y+dy]):
+            lowest_cost[x+dx][y+dy] = lowest_cost[x][y] + cost
+            neighbours.append(((x+dx,y+dy), d))
+    return neighbours
     
 def traverse(s : tuple[int, int], d : str):
-    new = add_surrounding_distances(s, d)
+    new = find_neighbours(s, d)
     for n,d in new:
         traverse(n, d)
 
-def subtract(from_tile : tuple[int, int], direction : str, tiles : defaultdict):
-    (x,y) = from_tile
-    if tiles[from_tile] >= 0:
+def reverse_and_subtract(start_tile : tuple[int, int], direction : str, tiles : defaultdict):
+    (x,y) = start_tile
+    if tiles[start_tile] > 0:
         for d in ["^",">","<", "v"] :
             (dx, dy) = directions[d]
             if (x+dx,y+dy) == start:
-                if tiles[from_tile] == cost(d, opposite[start_direction]):
+                if tiles[start_tile] == step_cost(d, opposite[start_direction]):
                     for p in tiles:
                         if tiles[p] > 0:
                             yield p
-            elif not walls[(x+dx, y+dy)] and distance_matrix[x+dx][y+dy] <= tiles[from_tile]: 
+            elif not walls[(x+dx, y+dy)] and lowest_cost[x+dx][y+dy] <= tiles[start_tile]: 
                 new_tiles = copy.deepcopy(tiles)
-                new_tiles[(x+dx,y+dy)] = tiles[from_tile] - cost(d, direction)
-                yield from subtract((x+dx,y+dy), d, new_tiles)
+                new_tiles[(x+dx,y+dy)] = tiles[start_tile] - step_cost(d, direction)
+                yield from reverse_and_subtract((x+dx,y+dy), d, new_tiles)
 
 def traverse_backwards():
     for d in ["^",">","<", "v"]:
         p = defaultdict(int)
         p[start] = -1
-        p[goal] = distance_matrix[goal[0]][goal[1]]
-        yield from subtract(goal,d, p)
+        p[goal] = lowest_cost[goal[0]][goal[1]]
+        yield from reverse_and_subtract(goal,d, p)
     yield goal
     yield start
 
 traverse(start, start_direction)
-print("Part 1: ", distance_matrix[goal[0]][goal[1]])
-distance_matrix[start[0]][start[1]] = 0
+print("Part 1: ", lowest_cost[goal[0]][goal[1]])
+lowest_cost[start[0]][start[1]] = 0
 print("Part 1: ", len(set(traverse_backwards())))
